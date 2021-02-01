c2e26c3bd11384ec7d35befa9deecc0094079ae7
Lightraine2 <laurencedale1986@gmail.com>
2021-02-01 19:02:58 +0000

    to 2.01.22

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Home.css';
 import Banner from './Banner';
+import Card from './Card';
+
 
 const Home = () => {
     return (
         <div className='home'>
             <Banner />
+            <div className='home__section'>
+            <Card
+                src={process.env.PUBLIC_URL + 'karate.jpg'}
+                title="Start Something New"
+                description="Improve your health, fitness and self confidence across a range of martial arts"
+            />
+            <Card
+                src={process.env.PUBLIC_URL + 'bjj-comp.jpg'}
+                title="Aim for Gold"
+                description="Competition focussed options for serious contenders."
+            />
+            <Card
+                src={process.env.PUBLIC_URL + 'tkd-school.jpg'}
+                title="For all the family"
+                description="Classes for juniors and parents"
+            />
+            </div>
+            <div className='home__section'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
